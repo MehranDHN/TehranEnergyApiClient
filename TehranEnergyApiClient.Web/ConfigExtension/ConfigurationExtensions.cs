cb0a1eef42451b041945afe3f6a5ffd55ba53668
Mehran Dhn <FloydianMehran@live.com>
2020-09-14 19:46:13 +0430

    Adding Configuration Extensions

---
+++ TehranEnergyApiClient.Web/ConfigExtension/ConfigurationExtensions.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TehranEnergyApiClient.Web.ConfigExtension
+{
+    public static class ConfigurationExtensions
+    {
+        public static bool IsEnableLoraWANGateway(this IConfiguration config) => config.GetValue<bool>("Features:App:EnableLoraWANGateway");
+        public static bool IsEnableExternalPowerSrcApi(this IConfiguration config) => config.GetValue<bool>("Features:App:EnableExternalPowerSrcApi");
+        public static string AppTitle(this IConfiguration config) => config.GetValue<string>("Features:HomePage:AppTitle");
+    }
+}
