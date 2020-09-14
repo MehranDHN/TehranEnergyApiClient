e75b95fb58f709a06f3ec0436ec6e6312fd55101
Mehran Dhn <FloydianMehran@live.com>
2020-09-14 11:57:37 +0430

    Adding Domain Model Classes and DTOs

---
+++ TehranEnergyApiClient.DomainModels/Models/PowerCounter.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TehranEnergyApiClient.DomainModels.Models
+{
+    public class PowerCounter
+    {
+        public int Pkid { get; set; }
+        public string FileNo { get; set; }
+        public string TagIdentifier { get; set; }
+        public string OwnerName { get; set; }
+        public string BodySerial { get; set; }
+        public string SubscribeNo { get; set; }
+        public string Ceof { get; set; }
+        public string ImgExtension { get; set; }
+        public string Description { get; set; }
+        public Nullable<bool> ArchiveFlag { get; set; }
+        public Nullable<int> LocationID { get; set; }
+        public Nullable<bool> LockFlag { get; set; }
+        public Nullable<int> CntType { get; set; }
+
+        public virtual BldProperty TargetBuilding { get; set; }
+
+        public virtual PowerSrcInfo RelatedInfo { get; set; }
+        [NotMapped]
+        public int RowSeq
+        {
+            get; set;
+        }
+        public bool HasImage
+        {
+            get
+            {
+                bool hasImage = false;
+                hasImage = !String.IsNullOrEmpty(this.ImgExtension) && this.ImgExtension.Trim().Length > 3;
+                return hasImage;
+            }
+
+        }
+    }
+}
---
+++ TehranEnergyApiClient.DomainModels/Models/PowerSrcPayment.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TehranEnergyApiClient.DomainModels.Models
+{
+    public class PowerSrcPayment
+    {
+        public string bill_identifier { get; set; }
+        public Nullable<decimal> payment_amt { get; set; }
+        public Nullable<int> bank_code { get; set; }
+        public Nullable<int> ref_code { get; set; }
+        public Nullable<int> chanel_type { get; set; }
+        public int Pkid { get; set; }
+        public Nullable<System.DateTime> payment_date { get; set; }
+    }
+}
