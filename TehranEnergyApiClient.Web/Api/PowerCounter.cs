f8c0938602d8867bee4912d2292b5dc77c0ddb71
Mehran Dhn <FloydianMehran@live.com>
2020-09-14 12:49:48 +0430

    Adding AutoMapper Profile and Our First Api Controller for PowerCounter

---
+++ TehranEnergyApiClient.Web/Api/PowerCounter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TehranEnergyApiClient.Web.CQRS.PowerCounter.Queries;
+
+namespace TehranEnergyApiClient.Web.Api
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PowerCounter : ControllerBase
+    {
+        private readonly IMediator _mediator;
+        public PowerCounter(IMediator mediator)
+        {
+            _mediator = mediator ?? throw new ArgumentNullException(nameof(mediator));
+        }
+        [HttpGet]
+        [Route("GetPowerCounters")]
+        public async Task<IActionResult> GetPowerCounters()
+        {
+            var counters = await _mediator.Send(new GetPowerCounterListQuery());
+            return Ok(counters);
+        }
+    }
+}
---
+++ TehranEnergyApiClient.Web/MapperProfile/EnergyProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TehranEnergyApiClient.DomainModels.DTO;
+using TehranEnergyApiClient.DomainModels.Models;
+
+namespace TehranEnergyApiClient.Web.MapperProfile
+{
+    public class EnergyProfile : Profile
+    {
+        public EnergyProfile()
+        {
+            CreateMap<PowerSrcInfo, PowerSrcInfoDto>().ForMember(dest => dest.buildingId, opt => opt.MapFrom(src => src.TargetCounter != null ? src.TargetCounter.LocationID : 0));
+        }
+    }
+}
